0949ef8f56ae0acfc3cb6d2a33535e9dce77782b
xinsanitiix <xinsanitiix@gmail.com>
2025-11-04 00:01:54 +0100

    trying to get netfliy to work

--- Random Trials/Joint _files/function changeDescription(event) {.ts
+++
@@ -1,8 +0,0 @@
-function changeDescription(event) {
-  event.preventDefault();
-  let cityElement = document.querySelector("#current-city");
-  let temperatureElement = document.querySelector(".current-temperature-value");
-  let currentTemperature = temperatureElement.innerHTML;
-  let changeDegree = document.querySelector(".current-temperature-unit");
-  changeDegree.innerHTML = currentTemperature;
-}
